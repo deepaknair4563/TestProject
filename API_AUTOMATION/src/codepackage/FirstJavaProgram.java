869fede09a150f5b4a2778a518544b80c7ff1fc9
Deepa <Deepa@Hom>
2020-04-06 00:52:10 +0530

    test

--- API_AUTOMATION/src/codepackage/FirstJavaProgram.java
+++ API_AUTOMATION/src/codepackage/FirstJavaProgram.java
@@ -3,6 +3,12 @@ package codepackage;
 public class FirstJavaProgram {
 
 	public static void main(String[] args) {
+	int a=999999999;
+	int b=5;
+	int i=a+b;
+	System.out.println("The value of i is " +i);
+	System.out.println("12345");
+	
 	}
 
 }
