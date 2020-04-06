738551cbbc704084dac0318e9a9594ab1f22deb0
Deepa <Deepa@Hom>
2020-04-06 21:01:48 +0530

    ttttt

--- API_AUTOMATION/src/codepackage/FirstJavaProgram.java
+++ API_AUTOMATION/src/codepackage/FirstJavaProgram.java
@@ -4,10 +4,15 @@ public class FirstJavaProgram {
 	
 	public static void main(String[] args) {
 	int a=9;
-	int b=5;
+	int b=9;
 	int age=a*b;
 	System.out.println("Age is "  +age);
-
+	age=b-20;
+System.out.println(age);
+char name = 'a';
+boolean d=true;
+System.out.println(a);
+System.out.println(a==b);
 
 	}
 
